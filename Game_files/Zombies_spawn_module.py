8d535d64664c1e65cb01a0896c6791fc632f02a8
3than-37 <3than.falk@gmail.com>
2026-06-26 11:51:01 -0400

    Update Zombies_spawn_module.py

---
+++ Game_files/Zombies_spawn_module.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+import random
+import Zombie_wave_module
+
+class Zombie_spawn:
+    def __init__(self, screen, columns, type):
+        self.screen = screen
+        self.columns = columns
+        self.type = type
+    
+    def draw(self):
+        if self.type == 1:
+            s
+            # TODO regular zombie
+
+        if self.type == 2:
+            s
+            # TODO regular zombie
+
+        if self.type == 3:
+            s
+            # TODO regular zombie
+
+        if self.type == 4:
+            s
+            # TODO regular zombie
+
+        pass
+
+    def move(self):
+        pass
